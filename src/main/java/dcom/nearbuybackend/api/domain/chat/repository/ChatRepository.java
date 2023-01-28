f0ae8afa56ef38c6101f776982b62c0e7c4e1994
lomayd <jinho10167@gmail.com>
2023-01-28 14:36:50 +0900

    feat: MongoDB Reactive + Web Flux

--- src/main/java/dcom/nearbuybackend/api/domain/chat/repository/ChatRepository.java
+++ src/main/java/dcom/nearbuybackend/api/domain/chat/repository/ChatRepository.java
@@ -2,8 +2,9 @@ package dcom.nearbuybackend.api.domain.chat.repository;
 
 import dcom.nearbuybackend.api.domain.chat.Chat;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChatRepository extends MongoRepository<Chat, String> {
+public interface ChatRepository extends ReactiveMongoRepository<Chat, String> {
 }
