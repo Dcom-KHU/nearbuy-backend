d56de830686057455f1f980eedaaf0fa522d635c
Choi Seongwon <vlflalektzja@naver.com>
2023-02-05 17:58:31 +0900

    feat: Exchange Post Get, Register, Modify

--- src/main/java/dcom/nearbuybackend/api/domain/post/controller/ExchangePostController.java
+++ src/main/java/dcom/nearbuybackend/api/domain/post/controller/ExchangePostController.java
@@ -1,11 +1,42 @@
 package dcom.nearbuybackend.api.domain.post.controller;
 
+import dcom.nearbuybackend.api.domain.post.dto.ExchangePostRequestDto;
+import dcom.nearbuybackend.api.domain.post.dto.ExchangePostResponseDto;
+import dcom.nearbuybackend.api.domain.post.service.ExchangePostService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Api(tags = {"Exchange Post Controller"})
+@RequestMapping("/api/post/exchange")
 @RestController
 @RequiredArgsConstructor
 public class ExchangePostController {
+    private final ExchangePostService exchangePostService;
+
+    @ApiOperation("교환 게시글 조회")
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<ExchangePostResponseDto.ExchangePostInfo> getExchangePost(@RequestParam Integer id) {
+        return ResponseEntity.ok(exchangePostService.getExchangePost(id));
+    }
+
+    @ApiOperation("교환 게시글 등록")
+    @PostMapping
+    public ResponseEntity<ExchangePostRequestDto.ExchangePostRegister> registerExchangePost(HttpServletRequest httpServletRequest, @RequestBody ExchangePostRequestDto.ExchangePostRegister exchangePost){
+        exchangePostService.registerExchangePost(httpServletRequest, exchangePost);
+        return ResponseEntity.ok().build();
+    }
+
+    @ApiOperation("교환 게시글 수정")
+    @PatchMapping
+    public ResponseEntity<ExchangePostRequestDto.ExchangePostModify> modifyExchangePost(HttpServletRequest httpServletRequest, @RequestParam Integer id, @RequestBody ExchangePostRequestDto.ExchangePostModify exchangePost) {
+        exchangePostService.modifyExchangePost(httpServletRequest, id, exchangePost);
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/dcom/nearbuybackend/api/domain/post/dto/ExchangePostResponseDto.java
+++ src/main/java/dcom/nearbuybackend/api/domain/post/dto/ExchangePostResponseDto.java
@@ -1,4 +1,49 @@
 package dcom.nearbuybackend.api.domain.post.dto;
 
+import dcom.nearbuybackend.api.domain.post.ExchangePost;
+import io.swagger.annotations.ApiModel;
+import lombok.*;
+
+import java.util.List;
+
 public class ExchangePostResponseDto {
+    @ApiModel(value = "교환 게시글 조회")
+    @Builder
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ExchangePostInfo {
+        private Integer id;
+        private String type;
+        private String title;
+        private String writer;
+        private String detail;
+        private List<String> image;
+        private Long time;
+        private String location;
+        private Boolean ongoing;
+        private List<String> tag;
+        private String target;
+
+        public static ExchangePostInfo of(ExchangePost exchangePost) {
+            List<String> imageList = List.of(exchangePost.getImage().split(","));
+            List<String> tagList = List.of(exchangePost.getTag().split(","));
+
+            return ExchangePostInfo.builder()
+                    .id(exchangePost.getId())
+                    .type("exchange")
+                    .title(exchangePost.getTitle())
+                    .writer(exchangePost.getWriter().getName())
+                    .detail(exchangePost.getDetail())
+                    .image(imageList)
+                    .time(exchangePost.getTime())
+                    .location(exchangePost.getLocation())
+                    .ongoing(exchangePost.getOngoing())
+                    .tag(tagList)
+                    .target(exchangePost.getTarget())
+                    .build();
+        }
+
+    }
 }
