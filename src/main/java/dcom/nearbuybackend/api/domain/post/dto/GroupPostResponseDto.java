a14ab2c4d5a6749eda78e52b8a9fbd0874c1666d
sschan99 <seungchan1219@gmail.com>
2023-02-17 11:47:30 +0900

    feat: Group Post Get, Register, Modify

--- src/main/java/dcom/nearbuybackend/api/domain/post/dto/GroupPostResponseDto.java
+++ src/main/java/dcom/nearbuybackend/api/domain/post/dto/GroupPostResponseDto.java
@@ -1,4 +1,60 @@
 package dcom.nearbuybackend.api.domain.post.dto;
 
+import dcom.nearbuybackend.api.domain.post.GroupPost;
+import io.swagger.annotations.ApiModel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class GroupPostResponseDto {
+    @ApiModel(value = "공구 게시글 조회")
+    @Builder
+    @Getter
+    @Setter
+    public static class GroupPostInfo {
+        private Integer id;
+        private String type;
+        private String title;
+        private String writer;
+        private String detail;
+        private List<String> image;
+        private Long time;
+        private String location;
+        private Boolean ongoing;
+        private List<String> tag;
+        private Integer groupPrice;
+        private Integer totalPeople;
+        private String distribute;
+        private List<Long> day;
+
+        public static GroupPostResponseDto.GroupPostInfo of(GroupPost groupPost) {
+
+            List<String> imageList = List.of(groupPost.getImage().split(","));
+            List<String> tagList = List.of(groupPost.getTag().split(","));
+            Stream<String> dayStream = Arrays.stream(groupPost.getDay().split(","));
+            List<Long> dayList = dayStream.map(Long::parseLong).collect(Collectors.toList());
+
+            return GroupPostInfo.builder()
+                    .id(groupPost.getId())
+                    .type("group")
+                    .title(groupPost.getTitle())
+                    .writer(groupPost.getWriter().getName())
+                    .detail(groupPost.getDetail())
+                    .image(imageList)
+                    .time(groupPost.getTime())
+                    .location(groupPost.getLocation())
+                    .ongoing(groupPost.getOngoing())
+                    .tag(tagList)
+                    .groupPrice(groupPost.getGroupPrice())
+                    .totalPeople(groupPost.getTotalPeople())
+                    .distribute(groupPost.getDistribute())
+                    .day(dayList)
+                    .build();
+        }
+    }
 }
--- src/main/java/dcom/nearbuybackend/api/domain/post/service/GroupPostService.java
+++ src/main/java/dcom/nearbuybackend/api/domain/post/service/GroupPostService.java
@@ -1,9 +1,94 @@
 package dcom.nearbuybackend.api.domain.post.service;
 
+import dcom.nearbuybackend.api.domain.post.GroupPost;
+import dcom.nearbuybackend.api.domain.post.dto.GroupPostRequestDto;
+import dcom.nearbuybackend.api.domain.post.dto.GroupPostResponseDto;
+import dcom.nearbuybackend.api.domain.post.repository.GroupPostRepository;
+import dcom.nearbuybackend.api.domain.user.User;
+import dcom.nearbuybackend.api.global.security.config.TokenService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class GroupPostService {
+
+    private final GroupPostRepository groupPostRepository;
+    private final TokenService tokenService;
+
+    /**
+     * 공구 게시글 조회
+     */
+    public GroupPostResponseDto.GroupPostInfo getGroupPost(Integer id) {
+        GroupPost groupPost = groupPostRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 id의 게시글을 찾을 수 없습니다.")
+        );
+
+        return GroupPostResponseDto.GroupPostInfo.of(groupPost);
+    }
+
+    /**
+     * 공구 게시글 등록
+     */
+    public void registerGroupPost(HttpServletRequest httpServletRequest, GroupPostRequestDto.GroupPostRegister post) {
+
+        User user = tokenService.getUserByToken(tokenService.resolveToken(httpServletRequest));
+
+        GroupPost groupPost = new GroupPost();
+        groupPost.setTitle(post.getTitle());
+        groupPost.setWriter(user);
+        groupPost.setDetail(post.getDetail());
+        groupPost.setImage(getJoinByComma(post.getImage()));
+        groupPost.setTime(System.currentTimeMillis());
+        groupPost.setLocation(post.getLocation());
+        groupPost.setOngoing(true);
+        groupPost.setTag(getJoinByComma(post.getTag()));
+        groupPost.setGroupPrice(post.getGroupPrice());
+        groupPost.setTotalPeople(post.getTotalPeople());
+        groupPost.setDistribute(post.getDistribute());
+        groupPost.setDay(post.getDay().stream().map(l -> Long.toString(l)).collect(Collectors.joining(",")));
+
+        groupPostRepository.save(groupPost);
+    }
+
+    private static String getJoinByComma(List<String> list) {
+        return String.join(",", list);
+    }
+
+    /**
+     * 공구 게시글 수정
+     */
+    public void modifyGroupPost(HttpServletRequest httpServletRequest, Integer id,
+                                GroupPostRequestDto.GroupPostModify post) {
+
+        User user = tokenService.getUserByToken(tokenService.resolveToken(httpServletRequest));
+
+        GroupPost groupPost = groupPostRepository.findById(id).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 id의 게시글을 찾을 수 없습니다."));
+
+        if (user.equals(groupPost.getWriter())) {
+            String imageList = getJoinByComma(post.getImage());
+            String tagList = getJoinByComma(post.getTag());
+
+            groupPost.setTitle(post.getTitle());
+            groupPost.setDetail(post.getDetail());
+            groupPost.setImage(imageList);
+            groupPost.setLocation(post.getLocation());
+            groupPost.setOngoing(post.getOngoing());
+            groupPost.setTag(tagList);
+            groupPost.setGroupPrice(post.getGroupPrice());
+            groupPost.setTotalPeople(post.getTotalPeople());
+
+            groupPostRepository.save(groupPost);
+        } else {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "게시물 수정 접근 권한이 없습니다.");
+        }
+    }
+
 }
