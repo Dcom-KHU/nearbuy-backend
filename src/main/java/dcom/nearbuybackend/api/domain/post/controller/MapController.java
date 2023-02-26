2467a89f98718fc1d49df7b5a4a04115f0708e6c
lomayd <jinho10167@gmail.com>
2023-02-26 11:35:30 +0900

    feat: Map Controller Request Mapping

--- src/main/java/dcom/nearbuybackend/api/domain/post/controller/MapController.java
+++ src/main/java/dcom/nearbuybackend/api/domain/post/controller/MapController.java
@@ -2,10 +2,12 @@ package dcom.nearbuybackend.api.domain.post.controller;
 
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Api(tags = {"Map Controller"})
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class MapController {
 }
