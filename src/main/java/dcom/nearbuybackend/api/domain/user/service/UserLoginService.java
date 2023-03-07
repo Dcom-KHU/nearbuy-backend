4aed2f1db19471f1ec8014241d78fb1957931110
Choi Seongwon <vlflalektzja@naver.com>
2023-03-07 15:58:53 +0900

    fix: change Comment

--- src/main/java/dcom/nearbuybackend/api/domain/user/service/UserLoginService.java
+++ src/main/java/dcom/nearbuybackend/api/domain/user/service/UserLoginService.java
@@ -70,8 +70,8 @@ public class UserLoginService {
             String tempPassword = getTempPassword();
             user.setPassword(tempPassword);
 
-            String email = "sw.choi@khu.ac.kr";
-            //        String email = user.getId();
+//            String email = "sw.choi@khu.ac.kr"; // test용 코드
+            String email = id;
             SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
             simpleMailMessage.setTo(email);
             simpleMailMessage.setSubject("Nearbuy 임시비밀번호 안내 이메일 입니다");
@@ -79,15 +79,15 @@ public class UserLoginService {
                     "회원님의 임시 비밀번호는 " + tempPassword + "입니다. 로그인 후에 비밀번호를 변경 해주세요.");
             userRepository.save(user);
             javaMailSender.send(simpleMailMessage);
-        }
-        else {
+        } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "해당하는 이메일이나 이름이 없습니다.");
         }
 
     }
-    private String getTempPassword(){
-        char[] charSet = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
-                'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+    private String getTempPassword() {
+        char[] charSet = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+                'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
         String str = "";
 
