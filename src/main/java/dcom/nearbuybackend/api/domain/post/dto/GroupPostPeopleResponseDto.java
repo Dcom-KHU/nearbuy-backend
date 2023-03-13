d6d34f6f8cec65dbb89491e1cc0be1a1c7a242bb
lomayd <jinho10167@gmail.com>
2023-03-13 06:07:25 +0000

    refactor: Group Post Swagger Detail

--- src/main/java/dcom/nearbuybackend/api/domain/post/dto/GroupPostPeopleResponseDto.java
+++ src/main/java/dcom/nearbuybackend/api/domain/post/dto/GroupPostPeopleResponseDto.java
@@ -3,6 +3,7 @@ package dcom.nearbuybackend.api.domain.post.dto;
 import dcom.nearbuybackend.api.domain.post.GroupPostPeople;
 import dcom.nearbuybackend.api.domain.user.User;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,12 +11,13 @@ import lombok.Setter;
 import java.util.List;
 
 public class GroupPostPeopleResponseDto {
-    @ApiModel(value = "공구 게시글 참여자 조회")
+    @ApiModel(value = "공구 게시글 참여자 정보")
     @Builder
     @Getter
     @Setter
     public static class GroupPostPeopleInfo {
 
+        @ApiModelProperty(value = "공구 게시글 참여자 리스트")
         private List<GroupPostPersonInfo> user;
 
         public static GroupPostPeopleInfo of(List<GroupPostPersonInfo> groupPostPersonInfos) {
@@ -26,14 +28,17 @@ public class GroupPostPeopleResponseDto {
         }
     }
 
-    @ApiModel(value = "공구 게시글 참여자별 조회")
+    @ApiModel(value = "공구 게시글 참여자별 정보)
     @Builder
     @Getter
     @Setter
     public static class GroupPostPersonInfo {
 
+        @ApiModelProperty(value = "이름(닉네임)")
         private String name;
+        @ApiModelProperty(value = "매너 온도")
         private Double mannerPoint;
+        @ApiModelProperty(value = "참여 가능 여부")
         private Boolean participate;
 
         public static GroupPostPersonInfo of(GroupPostPeople groupPostPeople) {
