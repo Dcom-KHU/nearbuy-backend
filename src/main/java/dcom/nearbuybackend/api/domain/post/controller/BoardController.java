67bf6e93646e65a4118cb450834257f48cb05581
lomayd <jinho10167@gmail.com>
2023-03-13 01:49:33 +0000

    refactor: Board Swagger Detail

--- src/main/java/dcom/nearbuybackend/api/domain/post/controller/BoardController.java
+++ src/main/java/dcom/nearbuybackend/api/domain/post/controller/BoardController.java
@@ -2,8 +2,7 @@ package dcom.nearbuybackend.api.domain.post.controller;
 
 import dcom.nearbuybackend.api.domain.post.dto.BoardResponseDto;
 import dcom.nearbuybackend.api.domain.post.service.BoardService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -22,15 +21,21 @@ public class BoardController {
 
     private final BoardService boardService;
 
-    @ApiOperation("게시판 조회")
+    @ApiOperation(value = "게시판 조회", notes = "타입별로 해당하는 게시글 리스트를 조회합니다.")
+    @ApiResponses ({
+            @ApiResponse(code = 400, message = "게시글 타입을 잘못 입력하셨습니다.")
+    })
     @GetMapping
-    public ResponseEntity<BoardResponseDto.BoardInfo> getBoard(@RequestParam String type, @PageableDefault(size = 9, sort = "time",  direction = Sort.Direction.DESC) Pageable pageable) {
+    public ResponseEntity<BoardResponseDto.BoardInfo> getBoard(@ApiParam(value = "게시글 타입(all, sale, exchange, free, auction, group)", required = true) @RequestParam String type, @PageableDefault(size = 9, sort = "time",  direction = Sort.Direction.DESC) Pageable pageable) {
         return ResponseEntity.ok(boardService.getBoard(type, pageable));
     }
 
-    @ApiOperation("게시판 검색")
+    @ApiOperation(value = "게시판 검색", notes = "타입별로 검색 내용이 포함되는 해당하는 게시글 리스트를 조회합니다.")
+    @ApiResponses ({
+            @ApiResponse(code = 400, message = "게시글 타입을 잘못 입력하셨습니다.")
+    })
     @GetMapping("/search")
-    public ResponseEntity<BoardResponseDto.BoardInfo> searchBoard(@RequestParam String type, @RequestParam String search, @PageableDefault(size = 9, sort = "time",  direction = Sort.Direction.DESC) Pageable pageable) {
+    public ResponseEntity<BoardResponseDto.BoardInfo> searchBoard(@ApiParam(value = "게시글 타입(all, sale, exchange, free, auction, group)", required = true) @RequestParam String type, @RequestParam String search, @PageableDefault(size = 9, sort = "time",  direction = Sort.Direction.DESC) Pageable pageable) {
         return ResponseEntity.ok(boardService.searchBoard(type, search, pageable));
     }
 }
