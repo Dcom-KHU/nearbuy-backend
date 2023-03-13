0d0503e7183456bcacffef93a92317206fe1017e
lomayd <jinho10167@gmail.com>
2023-03-14 03:41:48 +0900

    remove: Chat Controller Swagger

--- src/main/java/dcom/nearbuybackend/api/domain/chat/controller/ChatController.java
+++ src/main/java/dcom/nearbuybackend/api/domain/chat/controller/ChatController.java
@@ -2,8 +2,6 @@ package dcom.nearbuybackend.api.domain.chat.controller;
 
 import dcom.nearbuybackend.api.domain.chat.Chat;
 import dcom.nearbuybackend.api.domain.chat.service.ChatService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +10,6 @@ import reactor.core.publisher.Flux;
 
 import javax.servlet.http.HttpServletRequest;
 
-@Api(tags = {"Chat Controller"})
 @RestController
 @RequestMapping("/api/chat")
 @RequiredArgsConstructor
@@ -28,13 +25,11 @@ public class ChatController {
 
     private final ChatService chatService;
 
-    @ApiOperation("채팅방 안 채팅 목록 조회(SSE(Server-Sent Events) Protocol)")
     @GetMapping(value = "/list" ,produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public ResponseEntity<Flux<Chat>> getChatList(HttpServletRequest httpServletRequest, @RequestParam Integer room) {
         return ResponseEntity.ok(chatService.getChatList(httpServletRequest, room));
     }
 
-    @ApiOperation("채팅방 목록 조회(SSE(Server-Sent Events) Protocol)")
     @GetMapping(value = "/room" ,produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public ResponseEntity<Flux<Chat>> getChatRoomList(HttpServletRequest httpServletRequest) {
         return ResponseEntity.ok(chatService.getChatRoomList(httpServletRequest));
