48372ec0230650e487b34ca11d4221acb8542c38
Choi Seongwon <vlflalektzja@naver.com>
2023-03-28 23:17:56 +0900

    feat: 채팅 보내기 구현 (테스트 x)

--- src/main/java/dcom/nearbuybackend/api/domain/chat/controller/ChatController.java
+++ src/main/java/dcom/nearbuybackend/api/domain/chat/controller/ChatController.java
@@ -1,12 +1,14 @@
 package dcom.nearbuybackend.api.domain.chat.controller;
 
 import dcom.nearbuybackend.api.domain.chat.Chat;
+import dcom.nearbuybackend.api.domain.chat.dto.ChatRequestDto;
 import dcom.nearbuybackend.api.domain.chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -24,6 +26,11 @@ public class ChatController {
      */
 
     private final ChatService chatService;
+
+    @PostMapping(value = "/send")
+    public ResponseEntity<Mono<Chat>> sendChat(HttpServletRequest httpServletRequest, @RequestBody ChatRequestDto.sendChat sendChat) {
+        return ResponseEntity.ok(chatService.sendChat(httpServletRequest, sendChat));
+    }
 
     @GetMapping(value = "/list" ,produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public ResponseEntity<Flux<Chat>> getChatList(HttpServletRequest httpServletRequest, @RequestParam Integer room) {
--- src/main/java/dcom/nearbuybackend/api/domain/chat/service/ChatService.java
+++ src/main/java/dcom/nearbuybackend/api/domain/chat/service/ChatService.java
@@ -1,12 +1,14 @@
 package dcom.nearbuybackend.api.domain.chat.service;
 
 import dcom.nearbuybackend.api.domain.chat.Chat;
+import dcom.nearbuybackend.api.domain.chat.dto.ChatRequestDto;
 import dcom.nearbuybackend.api.domain.chat.repository.ChatRepository;
 import dcom.nearbuybackend.api.domain.user.User;
 import dcom.nearbuybackend.api.global.security.config.TokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,16 +22,29 @@ public class ChatService {
 
     private final TokenService tokenService;
 
+    public Mono<Chat> sendChat(HttpServletRequest httpServletRequest, ChatRequestDto.sendChat sendChat) {
+        User sender = tokenService.getUserByToken(tokenService.resolveToken(httpServletRequest));
+        int room = sendChat.getRoom();
+        String message = sendChat.getMessage();
+
+        Chat chat = new Chat();
+        chat.setRoom(room);
+        chat.setMessage(message);
+        chat.setSender(sender.getId());
+        chat.setTime(System.currentTimeMillis());
+        return chatRepository.save(chat);
+    }
+
     // 유저가 속해있는 채팅방 별 채팅 목록 조회
     public Flux<Chat> getChatList(HttpServletRequest httpServletRequest, Integer room) {
-        User user =  tokenService.getUserByToken(tokenService.resolveToken(httpServletRequest));
+        User user = tokenService.getUserByToken(tokenService.resolveToken(httpServletRequest));
         Flux<Chat> chatList = chatRepository.findAllByRoomAndUsers(room, user.getId());
         return chatList.subscribeOn(Schedulers.boundedElastic());
     }
 
     // 유저가 속해있는 채팅방 각각의 마지막 채팅 목록 조회
     public Flux<Chat> getChatRoomList(HttpServletRequest httpServletRequest) {
-        User user =  tokenService.getUserByToken(tokenService.resolveToken(httpServletRequest));
+        User user = tokenService.getUserByToken(tokenService.resolveToken(httpServletRequest));
         Flux<Chat> chatList = chatRepository.findAllByUsers(user.getId());
         return chatList.subscribeOn(Schedulers.boundedElastic());
     }
