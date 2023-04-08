e53571333ef97fff6ea459fe5ffd1129a95c2295
lomayd <jinho10167@gmail.com>
2023-04-08 22:37:34 +0900

    fix: Chat List, Chat Room List Get

--- src/main/java/dcom/nearbuybackend/api/domain/chat/repository/ChatRepository.java
+++ src/main/java/dcom/nearbuybackend/api/domain/chat/repository/ChatRepository.java
@@ -9,11 +9,11 @@ import java.util.List;
 public interface ChatRepository extends MongoRepository<Chat, String> {
 
     // 유저가 속해있는 채팅방 별 채팅 목록 확인
-    @Query("{room: ?0, userList: {$in: [?1]}}")
+    @Query("{room: ?0, userNameList: {$in: [?1]}}")
     List<Chat> findAllByRoomAndUser(Integer room, String user);
 
     // 유저가 속해있는 채팅방 각각의 마지막 채팅 목록 조회
-    @Query("{userList: {$in: [?0]}, last: true}")
+    @Query("{userNameList: {$in: [?0]}, last: true}")
     List<Chat> findAllByUser(String user);
 
     Chat findFirstByRoomOrderByTimeDesc(Integer room);
